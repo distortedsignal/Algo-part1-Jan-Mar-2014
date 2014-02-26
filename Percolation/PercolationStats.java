2eb8fc37b51ad2ccdaa535a6b9e254b9ba290128
Tom Kelley <tkelley@axolotl.com>
2014-02-26 10:59:52 -0800

    Adding methods to PercolateStats.java. Lots to do.

--- Percolation/PercolationStats.java
+++ Percolation/PercolationStats.java
@@ -5,4 +5,52 @@
  */
 public class PercolationStats {
     
+    /**
+     * Perform T independent computational experiments on an N-by-N grid.
+     * @param N
+     * @param T 
+     */
+    public PercolationStats(int N, int T) {
+        
+    }
+    
+    /**
+     * Sample mean of percolation threshold.
+     * @return 
+     */
+    public double mean() {
+        return 0.0;
+    }
+    
+    /**
+     * Sample Standard Deviation of percolation threshold.
+     * @return 
+     */
+    public double stddev() {
+        return 0.0;
+    }
+    
+    /**
+     * Returns lower bound of the 95% confidence interval.
+     * @return 
+     */
+    public double confidenceLo() {
+        return 0.0;
+    }
+    
+    /**
+     * Returns upper bound of the 95% confidence interval.
+     * @return 
+     */
+    public double confidenceHi() {
+        return 0.0;
+    }
+    
+    /**
+     * Test client.
+     * @param main 
+     */
+    public static void main(String[] main) {
+        
+    }
 }
